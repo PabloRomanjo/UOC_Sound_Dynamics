ad83934e3638516cee41d59fd48b93fd3d2c0e94
Pablo Román <93191947+PabloRomanjo@users.noreply.github.com>
2022-01-10 21:04:46 +0100

    final update

--- filter/eda.py
+++ filter/eda.py
@@ -8,7 +8,7 @@ def count_track_artist(df, artist):
     :param artist: Nombre de un artista (ej: 'Extremoduro')
     :return: Número de canciones del artísta especificado
     """
-    
+
     tracks = df[df['artist_name'] == artist]
     return print("Tracks recorded by {}:".format(artist), len(tracks))
 
@@ -20,20 +20,21 @@ def count_track_word(df, word):
     :param word: Una palabra (ej: 'police')
     :return: Número de canciones con la palabra especificada.
     """
-    
+
     tracks = df[df['name'].str.contains(word)]
     return print("Name of tracks including the string {}:".format(word),
                  len(tracks))
 
 
 def count_track_decade(df, decade):
-    """Cuenta el número de canciones de los albumes publicados en una década especifica
+    """Cuenta el número de canciones de los albumes publicados en
+    una década especifica
 
     :param df: Dataframe incluyendo tracks, artists y albums
     :param decade: Decada (ej: 1990)
     :return: Número de albumes en la decada especificada
     """
-    
+
     if decade % 10 == 0:
         tracks = df[df['album_release_year'].between(decade, decade + 9)]
         return print("Number of tracks published in {}'s decade:".format(decade),
@@ -46,17 +47,20 @@ todays_year = date.today().year
 
 
 def most_popular_track(df, year):
-    """ Devuelve una lista con las canciones más populares en los últimos x años
+    """ Devuelve una lista con las canciones más populares en los
+    últimos x años
 
     :param df: Dataframe incluyendo tracks, artists y albums
     :param year: Número de años (ej: 10)
-    :return: Devuelve una lista con las canciones más populares de los últimos años
+    :return: Devuelve una lista con las canciones más populares de los
+    últimos años
     """
-    
-    tracks = df[df['album_release_year'].between(todays_year - year, todays_year)]
-    mostpopular = tracks[tracks['popularity'] == max(tracks['popularity'])]['name']
+
+    tracks = df[df['album_release_year'].between(todays_year - year,
+                                                 todays_year)]
+    mp = tracks[tracks['popularity'] == max(tracks['popularity'])]['name']
     return print("The most popular track in the last {} years is".format(year),
-                 list(mostpopular)[0])
+                 list(mp)[0])
 
 
 def active_artists(df, decade):
@@ -66,9 +70,9 @@ def active_artists(df, decade):
     :param decade: Década (ej: 1960)
     :return: Lista de artistas en activo
     """
-    
     if decade % 10 == 0:
-        df['decade'] = [year - (year % 10) for year in df["album_release_year"]]
+        df['decade'] = [year - (year % 10) for year
+                        in df["album_release_year"]]
         subdf = df[['artist_name', 'decade']]
         artists = list(set(subdf[subdf['decade'] == decade]['artist_name']))
         if len(artists) == 0:
@@ -84,6 +88,7 @@ def active_artists(df, decade):
                         test.append("no")
                 if "no" not in test:
                     selected.append(artists[artist])
-            return print("Active artists since {}'s decade:".format(decade), selected)
+            return print("Active artists since {}'s decade:".format(decade),
+                         selected)
     else:
         return print('Please, select a correct decade (e.g. 1970, 1990, 2000)')
